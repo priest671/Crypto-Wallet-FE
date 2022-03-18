515ca8a65fe0afbd3e85bd02a351642381973586
Abdullah Raheel <Abdullaharaheel@gmail.com>
2022-03-18 20:04:41 +0500

    First Commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'crypto-wallet-fe',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
