755d1086357255a2b87eb0f75796fd9ab36e6015
Abdullah Raheel <Abdullaharaheel@gmail.com>
2022-04-08 12:11:36 +0500

    Update WalletActions.tsx

--- src/store/Wallet/WalletActions.tsx
+++ src/store/Wallet/WalletActions.tsx
@@ -23,40 +23,67 @@ export const getWalletAPI = (token: string) => {
   };
 };
 
-const getTotalBalance = async (coins: any) => {
-  const sendRequest = async (acronym: string) => {
-    return await axios.get(`https://api.coinbase.com/v2/prices/${acronym}-PKR/buy`);
-  };
+// const getPrice = async (acronym: string) => {
+//   const sendRequest = async (acronym: string) => {
+//     return await axios.get(`https://api.coinbase.com/v2/prices/${acronym}-PKR/buy`);
+//   };
 
-  let totalBalance = 0;
-  coins.forEach(async (coin: any) => {
-    if (coin.coin.acronym !== "PKR") {
-      const response = await sendRequest(coin.coin.acronym);
-        let price = response.data.data.amount * coin.quantity;
-        price = parseFloat(price.toFixed(2));
-        totalBalance += price;
-        console.log("not PKR", totalBalance);
-      
-    } else if (coin.coin.acronym === "PKR") {
-      totalBalance += parseFloat(coin.quantity);
-      console.log("PKR", totalBalance);
-    }
-  });
+//   try {
+//     await sendRequest(acronym).then((res) => {
+//       // console.log(res.data.data.amount);
+//       return res.data.data.amount;
+//     });
+//     // let price = response.data.data.amount * coin.quantity;
+//     // price = parseFloat(price.toFixed(2));
+//     // totalBalance += price;
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
 
-  return totalBalance;
-}
+// export const updateBalance = (coins: any) => {
+//   return async (dispatch: any) => {
+//     try {
+//       coins.forEach(async (coin: any) => {});
+//       //TODO: make this async
+
+//       let totalBalance = 0;
+//       for (let coin of coins) {
+//         let quantity = coin.quantity;
+
+//         if (coin.coin.acronym !== "PKR") {
+//           let price: any = await getPrice(coin.acronym).then(
+
+//             price = Number(price.toFixed(2));
+//             totalBalance += price * quantity;
+
+//           );
+//         }
+
+//         let price = await getPrice(coin.coin.acronym).then((res) => {
+//           console.log(price);
+//           console.log("quantity", quantity);
+//         });
+//       }
+//       await getPrice(coins).then((res) => {
+//         console.log("res 2:", res);
+//       });
+
+//       console.log("dispatching...");
+//       dispatch(walletActions.setBalance(totalBalance));
+//     } catch (err: any) {
+//       let error = decodeError(err);
+//       throw error;
+//     }
+//   };
+// };
 
 export const updateBalance = (coins: any) => {
   return async (dispatch: any) => {
     try {
-      //TODO: make this async
-
-      let totalBalance: any;
-      totalBalance = await getTotalBalance(coins);
-
-
-     console.log("dispatching...");
-      dispatch(walletActions.setBalance(totalBalance));
+      console.log("update Balance...");
+      console.log(coins);
+      // dispatch(walletActions.setBalance(totalBalance));
     } catch (err: any) {
       let error = decodeError(err);
       throw error;
